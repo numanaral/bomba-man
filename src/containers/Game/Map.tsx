e22412d97d6ea3ea02bd8b511d49b348729e758b
Numan <ahmet.aral@ucalgary.ca>
2021-03-22 09:47:51 -0600

    map/wrapper for the game

--- src/containers/Game/Map.tsx
+++ src/containers/Game/Map.tsx
@@ -1,19 +1,51 @@
 import styled from 'styled-components';
 import theme from 'theme';
+import Tile from './Tile';
 
-const Map = styled.div`
-	width: 800px;
-	height: 500px;
+const TILE_SIZE = 32;
+
+interface Props {
+	size: RangeOf<15>;
+	collisionCoordinates?: CollisionCoordinates;
+	children: React.ReactNode;
+}
+
+const Wrapper = styled.div<StyledProps<Props, 'size'>>`
+	width: ${({ $size }) => `calc(${theme.game.character.size} * ${$size})`};
+	height: ${({ $size }) => `calc(${theme.game.character.size} * ${$size})`};
 	border-radius: ${theme.shape.borderRadius};
 	background-color: ${theme.palette.background.secondary};
+	position: relative;
+	box-shadow: 0 10px 20px rgba(0, 0, 0, 0.5), 0 6px 6px rgba(0, 0, 0, 0.6);
 `;
 
-// const Map = () => {
-// 	return (
-// 		<div>
-// 			<p>Map</p>
-// 		</div>
-// 	);
-// };
+const Map = ({ size, collisionCoordinates = {}, children }: Props) => {
+	const map = Array(size).fill(Array(size).fill(0));
+
+	let collisionIndex = 1;
+	return (
+		<Wrapper $size={size}>
+			{map.map((outer: Array<number>, outerInd) => {
+				return outer.map((_, innerInd) => {
+					const hasCollision =
+						collisionCoordinates[innerInd] === outerInd;
+					return (
+						<Tile
+							key={`${outerInd}_${innerInd}`}
+							size={TILE_SIZE}
+							top={outerInd * TILE_SIZE}
+							left={innerInd * TILE_SIZE}
+							{...(hasCollision && {
+								color: theme.palette.color.secondary,
+								collisionIndex: collisionIndex++,
+							})}
+						/>
+					);
+				});
+			})}
+			{children}
+		</Wrapper>
+	);
+};
 
 export default Map;
