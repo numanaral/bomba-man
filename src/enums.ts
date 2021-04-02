c5b7bec4ce2e90e1ec9e39b6093e0f00f4cb6f80
Numan <ahmet.aral@ucalgary.ca>
2021-04-02 15:58:01 -0600

    enums for types that can fill the squares

--- src/enums.ts
+++ src/enums.ts
@@ -16,4 +16,24 @@ enum ExplosionState {
 	Exploded = 'Exploded',
 }
 
-export { Direction, Axis, ExplosionState };
+enum Player {
+	Player1 = 'P1',
+	Player2 = 'P2',
+	Npc1 = 'P3',
+	Npc2 = 'P4',
+	Npc3 = 'P5',
+}
+
+enum Tile {
+	Empty = 'T1',
+	Breaking = 'T2',
+	NonBreaking = 'T3',
+}
+
+enum PowerUp {
+	BombSize = 'PU1',
+	Speed = 'PU2',
+	Invincibility = 'PU3',
+}
+
+export { Direction, Axis, ExplosionState, Player, Tile, PowerUp };
