55a4b49eae8ec0f5473b60442b38f91efa0308b0
Numan <ahmet.aral@ucalgary.ca>
2021-04-05 04:33:16 -0600

    ValuesOf ts utility for grabbing values of objects

--- src/global.d.ts
+++ src/global.d.ts
@@ -18,6 +18,30 @@ declare type StyledProps<T, Keys> = {
  */
 declare type KeysOf<T> = keyof T;
 
+/**
+ * Get the values of the object with const assertion as a type.
+ * - `{} as const`
+ * - `<const>{}`
+ * - `[] as const`
+ * - `<const>[]`
+ *
+ * @example
+ * ```ts
+ * // note the "as const"
+ * const AXIS = {
+ * 	X: 'xVal',
+ * 	Y: 'yVal',
+ * } as const;
+ *
+ * // 'xVal' | 'yVal'
+ * ValuesOf<typeof AXIS>
+ *
+ * const NUMBERS = [1, 2] as const;
+ * // 1 | 2
+ * ValuesOf<typeof NUMBERS>
+ * ```
+ */
+declare type ValuesOf<T> = Exclude<T[KeysOf<T>], Function>;
 
 declare type BaseColorVariant = 'primary' | 'secondary';
 declare type AlertColorVariant = 'error' | 'success' | 'warning' | 'info';
