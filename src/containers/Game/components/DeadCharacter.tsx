9133b667a5725bfd7cacaded5ae73e88b472e69c
Numan <ahmet.aral@ucalgary.ca>
2021-04-27 12:45:49 -0600

    wrap both dead character and powerup under TileIcon

--- src/containers/Game/components/DeadCharacter.tsx
+++ src/containers/Game/components/DeadCharacter.tsx
@@ -4,37 +4,13 @@ import {
 	FontAwesomeIconProps,
 	TopLeftCoordinates,
 } from 'containers/Game/types';
-
-import styled, { css, keyframes } from 'styled-components';
-import config from 'config';
 import theme from 'theme';
+import config from 'config';
+import TileIcon from './TileIcon';
 
 const DeadIcon = (props: FontAwesomeIconProps) => (
 	<FontAwesomeIcon icon={faBookDead} {...props} />
 );
-
-/** @see https://codepen.io/nelledejones/pen/gOOPWrK#L68 */
-const bounceAnimation = keyframes`
-	0% { transform: scale(0, 0);  }
-	25% { transform: scale(0.9, 1.1);  }
-	50% { transform: scale(1.1, 0.9);  }
-	75% { transform: scale(0.95, 1.05); }
-	100% { transform: scale(1, 1); opacity: 1; }
-`;
-
-const Wrapper = styled.div`
-	position: absolute;
-	width: ${config.size.tile}px;
-	height: ${config.size.tile}px;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	opacity: 0;
-	${css`
-		animation: ${bounceAnimation} var(--block-animation-duration) ease 1.3s
-			forwards;
-	`}
-`;
 
 interface Props {
 	coordinates: TopLeftCoordinates;
@@ -42,9 +18,14 @@ interface Props {
 
 const DeadCharacter = ({ coordinates: { top, left } }: Props) => {
 	return (
-		<Wrapper style={{ top, left }}>
+		<TileIcon
+			$top={top}
+			$left={left}
+			$size={config.size.character}
+			$animationDelay={`${config.duration.bomb.exploding}s`}
+		>
 			<DeadIcon color={theme.palette.color.default} />
-		</Wrapper>
+		</TileIcon>
 	);
 };
 
