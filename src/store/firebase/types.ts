0132828b7a4ae25de36e0c71c1a100a4d19bc749
Numan <ahmet.aral@ucalgary.ca>
2021-04-28 17:13:40 -0600

    hook to simplfy firebase functions, has type safety

--- src/store/firebase/types.ts
+++ src/store/firebase/types.ts
@@ -13,9 +13,49 @@ namespace FirebaseObjects {
 
 	export type Generic = Record<string, any>;
 }
-type FirebaseGenericObject = Record<string, any>;
-type FirebaseObjectKeys = Array<string>;
-// TODO: Once we have react-router and auth/roles setup, update this
+
+namespace FirebaseUtils {
+	export type OverridableType<
+		Default,
+		Override = void
+	> = Override extends void ? Default : Override;
+
+	export type UpdateableValue = string | number | boolean | object;
+	export type OverridableValue = UpdateableValue | void;
+
+	export type RefSubPath = `/${string}`;
+
+	export type PromiseCallback = {
+		onSuccess?: (data: DataSnapshot) => void;
+		onError?: (err: Error) => void;
+	};
+
+	export type DataSnapshotPromise = Promise<DataSnapshot | Error>;
+	/** @see https://github.com/Microsoft/TypeScript/issues/12776#issuecomment-265885846 */
+	export const DataSnapshotPromise = Promise;
+
+	export type CreateOrUpdateProps<RootSchema, OverrideSchema> = [
+		newProps: Partial<OverridableType<RootSchema, OverrideSchema>>,
+		subPath?: RefSubPath,
+		cb?: PromiseCallback
+	];
+
+	export type CreateProps<RootSchema, OverrideSchema> = CreateOrUpdateProps<
+		RootSchema,
+		OverrideSchema
+	>;
+
+	export type ReadProps<RootSchema, OverrideSchema> = [
+		state: OverridableType<RootSchema, OverrideSchema>
+	];
+
+	export type UpdateProps<RootSchema, OverrideSchema> = CreateOrUpdateProps<
+		RootSchema,
+		OverrideSchema
+	>;
+
+	export type RemoveProps = [subPath?: RefSubPath, cb?: PromiseCallback];
+}
 
 namespace FirebaseSchema {
 	// TODO: Once we have react-router and auth/roles setup, update this
@@ -31,4 +71,4 @@ namespace FirebaseSchema {
 	export type FirestoreSchema = FirebaseObjects.Generic;
 }
 
-export type { FirebaseObjects, FirebaseSchema };
+export type { FirebaseObjects, FirebaseUtils, FirebaseSchema };
