b770c57032937f88101e3b1a81a95e5980ca512c
Numan <ahmet.aral@ucalgary.ca>
2021-05-10 16:19:25 -0600

    useInterval returns a stopper now

--- src/hooks/useInterval.tsx
+++ src/hooks/useInterval.tsx
@@ -8,15 +8,22 @@ type Callback = () => void;
  * @see https://overreacted.io/making-setinterval-declarative-with-react-hooks/#just-show-me-the-code
  *
  * @param callback Method to call in each interval.
- * @param delay Delay for interval.
+ * @param delay Delay for interval (in milliseconds).
+ *
+ * @returns stopper for the interval
  */
 const useInterval = (callback: Callback, delay: number) => {
 	const savedCallback = useRef<Callback>();
+	const id = useRef<NodeJS.Timeout>();
 
 	// Remember the latest callback.
 	useEffect(() => {
 		savedCallback.current = callback;
 	}, [callback]);
+
+	const clearLastInterval = () => {
+		clearInterval(id.current as NodeJS.Timeout);
+	};
 
 	// Set up the interval.
 	useEffect(() => {
@@ -24,9 +31,11 @@ const useInterval = (callback: Callback, delay: number) => {
 			savedCallback.current?.();
 		};
 
-		const id = setInterval(tick, delay);
-		return () => clearInterval(id);
+		id.current = setInterval(tick, delay);
+		return () => clearLastInterval();
 	}, [delay]);
+
+	return clearLastInterval;
 };
 
 export default useInterval;
