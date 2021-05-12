f5455e31ea713448e9e341839bf54efc7e316125
Numan <ahmet.aral@ucalgary.ca>
2021-05-11 20:59:48 -0600

    show join link on the waiting room

--- src/routes/pages/WaitingRoom.tsx
+++ src/routes/pages/WaitingRoom.tsx
@@ -3,6 +3,7 @@ import { RouteComponentPropsWithLocationState } from 'routes/types';
 import WaitingRoom from 'containers/WaitingRoom';
 import { H1, H4 } from 'components/typography';
 import theme from 'theme';
+import Spacer from 'components/Spacer';
 
 interface Props extends RouteComponentPropsWithLocationState<{ id: string }> {}
 
@@ -12,12 +13,24 @@ const WaitingRoomPage = ({
 		params: { id },
 	},
 }: Props) => {
+	const {
+		location: { origin, pathname },
+	} = window;
+	const link = origin + pathname;
 	return (
 		<PageContainer>
 			<H1> Waiting Room </H1>
+			<Spacer />
 			<H4>
-				Room / Game Id:{' '}
+				Room / Game Id:
+				<br />
 				<span style={{ color: theme.palette.color.info }}>{id}</span>
+			</H4>
+			<Spacer />
+			<H4>
+				Join Link:
+				<br />
+				<span style={{ color: theme.palette.color.info }}>{link}</span>
 			</H4>
 			<WaitingRoom gameId={id} />
 		</PageContainer>
