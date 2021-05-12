d25e2af114b072f1ba80dc74493e9a02ace38b38
Numan <ahmet.aral@ucalgary.ca>
2021-05-12 13:21:02 -0600

    remove online game on game end

--- src/hooks/useOnGameEnd.tsx
+++ src/hooks/useOnGameEnd.tsx
@@ -1,15 +1,18 @@
-import { GameEnd, PlayerId } from 'containers/Game/types';
+import { GameEnd, OnlineGameId, PlayerId } from 'containers/Game/types';
 import { GameEndCondition, PlayerCondition } from 'enums';
 import { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { BASE_PATH } from 'routes/constants';
 import { ReactRouterState } from 'routes/types';
+import useOnlineGameActions from 'store/firebase/hooks/useOnlineGameActions';
 
 const useOnGameEnd = (
 	players: GameEnd['players'],
-	currentOnlinePlayerId?: PlayerId
+	currentOnlinePlayerId?: PlayerId,
+	gameId?: OnlineGameId
 ) => {
 	const { push } = useHistory<ReactRouterState>();
+	const { removeOnlineGame } = useOnlineGameActions();
 
 	let gameEndCondition = GameEndCondition.Lose;
 	if (players && currentOnlinePlayerId) {
@@ -33,7 +36,22 @@ const useOnGameEnd = (
 				gameEndCondition,
 			},
 		});
-	}, [currentOnlinePlayerId, gameEndCondition, players, push]);
+
+		if (gameId) {
+			// delay the game deletion as the death event is recreating the
+			// object in firebase
+			setTimeout(() => {
+				removeOnlineGame(gameId);
+			}, 1000);
+		}
+	}, [
+		currentOnlinePlayerId,
+		gameEndCondition,
+		gameId,
+		players,
+		push,
+		removeOnlineGame,
+	]);
 };
 
 export default useOnGameEnd;
