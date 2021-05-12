47b81cfe8f74311e9d66978ce7da23bc3e21867a
Numan <ahmet.aral@ucalgary.ca>
2021-05-11 19:39:21 -0600

    display the url in NotFound

--- src/routes/pages/NotFound.tsx
+++ src/routes/pages/NotFound.tsx
@@ -1,12 +1,25 @@
+import { Link } from '@material-ui/core';
 import ContainerWithCenteredItems from 'components/ContainerWithCenteredItems';
 import PageContainer from 'components/PageContainer';
-import { H1 } from 'components/typography';
+import { H1, H4 } from 'components/typography';
+import { Link as NavLink } from 'react-router-dom';
+import { RouteComponentPropsWithLocationState } from 'routes/types';
 
-const NotFound = () => {
+const NotFound = ({ location }: RouteComponentPropsWithLocationState) => {
+	const referrer = location?.state?.referrer;
 	return (
 		<PageContainer fullHeight>
 			<ContainerWithCenteredItems vertical>
-				<H1>The page you were looking for is not found.</H1>
+				<H1>
+					The page you were looking for is not found.
+					{referrer && (
+						<H4>
+							<Link component={NavLink} to={referrer}>
+								{referrer}
+							</Link>
+						</H4>
+					)}
+				</H1>
 			</ContainerWithCenteredItems>
 		</PageContainer>
 	);
