c907d4e2b5c2eaa9e5e51f56fe656a77e67cb6d3
Numan <ahmet.aral@ucalgary.ca>
2021-05-13 13:29:39 -0600

    instructions page with moving player and key highligh

--- src/routes/pages/Instructions.tsx
+++ src/routes/pages/Instructions.tsx
@@ -1,12 +1,15 @@
+import gameConfig from 'config';
 import PageContainer from 'components/PageContainer';
 import { faKeyboard } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
 	FontAwesomeIconProps,
+	KeyboardEventCode,
+	KeyMap,
 	PlayerActionKeys,
 	PlayerId,
 } from 'containers/Game/types';
-import { H1 } from 'components/typography';
+import { H1, H4 } from 'components/typography';
 import {
 	Grid,
 	GridJustification,
@@ -17,23 +20,40 @@ import Container from 'components/Container';
 import styled from 'styled-components';
 import { CharacterIcon } from 'containers/RoomCreator/icons';
 import Spacer from 'components/Spacer';
+import theme from 'theme';
+import * as KeyCode from 'keycode-js';
+import { useKeyboardEvent } from 'store/redux/hooks/usePlayerEvents';
+import { useState } from 'react';
 
 const KeyboardIcon = (props: FontAwesomeIconProps) => (
 	<FontAwesomeIcon icon={faKeyboard} {...props} />
 );
 
+type IsPressed = { isPressed?: boolean };
+
 const KeyboardIconWrapper = styled(Container)<
-	StyledProps<Required<SquareIconProps>>
+	StyledProps<Required<SquareIconProps> & IsPressed>
 >`
 	border: 2px solid;
 	border-radius: 4px;
 	padding: 10px;
 	font-weight: bold;
 	height: 45px;
-	${({ $length, $size }) => {
+	transition: 0.3s;
+
+	${({ $length, $size, $isPressed }) => {
 		return `
 			height: ${$size}px;
 			width: ${$size * $length}px;
+			${
+				$isPressed
+					? `
+						background: white;
+						color: ${theme.palette.background.primary};
+						border: 2px dashed ${theme.palette.background.primary};
+				`
+					: ''
+			}
 		`;
 	}}
 `;
@@ -43,36 +63,68 @@ interface SquareIconProps {
 	length?: number;
 }
 
-const SquareIcon: React.FC<SquareIconProps> = ({
+const SquareIcon: React.FC<SquareIconProps & IsPressed> = ({
 	children,
 	size = 45,
 	length = 1,
+	isPressed,
 }) => (
-	<KeyboardIconWrapper $length={length} $size={size}>
+	<KeyboardIconWrapper $length={length} $size={size} $isPressed={isPressed}>
 		{children}
 	</KeyboardIconWrapper>
 );
 
 interface KeyboardSetupContainerProps {
-	keyboardConfig: Record<PlayerActionKeys, string>;
+	keyboardConfig: Record<
+		PlayerActionKeys,
+		{
+			label: string;
+			keyCode: KeyboardEventCode;
+		}
+	>;
 }
 
 const KeyboardSetupContainer = ({
 	keyboardConfig,
 }: KeyboardSetupContainerProps) => {
 	const { DropBomb, MoveDown, MoveLeft, MoveRight, MoveUp } = keyboardConfig;
-	const bombKeyLength = DropBomb.length > 1 ? 3 : 1;
+	const bombKeyLength = DropBomb.label.length > 1 ? 3 : 1;
+
+	const [keyMap, setKeyMap] = useState<KeyMap>({});
+	useKeyboardEvent({
+		onKeyDown: keyEventCode => {
+			setKeyMap(v => ({
+				...v,
+				[keyEventCode]: true,
+			}));
+		},
+		onKeyUp: keyEventCode => {
+			setKeyMap(v => ({
+				...v,
+				[keyEventCode]: false,
+			}));
+		},
+	});
 
 	return (
 		<Grid container>
 			<Grid container style={{ width: 45 * 3 }}>
 				<Grid container item xs={12} justify="center">
-					<SquareIcon>{MoveUp}</SquareIcon>
+					<SquareIcon isPressed={keyMap[MoveUp.keyCode]}>
+						{MoveUp.label}
+					</SquareIcon>
 				</Grid>
 				<Grid container item xs={12}>
-					{[MoveLeft, MoveDown, MoveRight].map(key => (
-						<SquareIcon key={key}>{key}</SquareIcon>
-					))}
+					{[MoveLeft, MoveDown, MoveRight].map(
+						({ label, keyCode }) => (
+							<SquareIcon
+								isPressed={keyMap[keyCode as KeyboardEventCode]}
+								key={label}
+							>
+								{label}
+							</SquareIcon>
+						)
+					)}
 				</Grid>
 			</Grid>
 			<Grid style={{ width: 10 }} />
@@ -82,7 +134,12 @@ const KeyboardSetupContainer = ({
 				justify="center"
 				alignItems="flex-end"
 			>
-				<SquareIcon length={bombKeyLength}>{DropBomb}</SquareIcon>
+				<SquareIcon
+					isPressed={keyMap[DropBomb.keyCode]}
+					length={bombKeyLength}
+				>
+					{DropBomb.label}
+				</SquareIcon>
 			</Grid>
 		</Grid>
 	);
@@ -98,10 +155,10 @@ const PlayerSetupContainer = ({
 	playerId,
 	side = 'left',
 }: PlayerSetupContainerProps) => {
-	const smAndDown = useMediaQuery<Theme>(theme =>
-		theme.breakpoints.down('sm')
-	);
+	const smAndDown = useMediaQuery<Theme>(t => t.breakpoints.down('sm'));
 	const isLeft = side === 'left';
+
+	const playerIdIndex = Number(playerId.replace('P', '')) - 1;
 
 	return (
 		<Grid container {...(!isLeft && { justify: 'flex-end' })}>
@@ -111,9 +168,11 @@ const PlayerSetupContainer = ({
 				xs={12}
 				md={6}
 				justify={
-					`flex-${
-						smAndDown || isLeft ? 'start' : 'end'
-					}` as GridJustification
+					smAndDown
+						? 'center'
+						: (`flex-${
+								isLeft ? 'start' : 'end'
+						  }` as GridJustification)
 				}
 			>
 				<Grid item {...(!isLeft && { style: { order: 3 } })}>
@@ -123,6 +182,9 @@ const PlayerSetupContainer = ({
 						id={playerId}
 						showId
 						isWalking
+						keyboardConfig={{
+							'0': gameConfig.keyboardConfig[playerIdIndex],
+						}}
 					/>
 				</Grid>
 				<Grid item>
@@ -138,28 +200,64 @@ const Instructions = () => {
 		<PageContainer style={{ overflow: 'hidden' }}>
 			<Grid container justify="center" alignItems="center">
 				<KeyboardIcon size="3x" /> &nbsp;&nbsp;
-				<H1>Instruction</H1>
+				<H1> Instructions </H1>
+				<H4 style={{ width: '100%' }}>
+					NOTE: In an online game, you can use both setup
+				</H4>
+				<H4 style={{ width: '100%' }}>
+					NOTE2: You can test the keys on this screen :)
+				</H4>
 			</Grid>
 			<Spacer spacing="10" />
 			<PlayerSetupContainer
 				playerId="P1"
 				keyboardConfig={{
-					MoveUp: 'W',
-					MoveLeft: 'A',
-					MoveDown: 'S',
-					MoveRight: 'D',
-					DropBomb: 'Space',
+					MoveUp: {
+						label: 'W',
+						keyCode: KeyCode.CODE_W,
+					},
+					MoveRight: {
+						label: 'D',
+						keyCode: KeyCode.CODE_D,
+					},
+					MoveDown: {
+						label: 'S',
+						keyCode: KeyCode.CODE_S,
+					},
+					MoveLeft: {
+						label: 'A',
+						keyCode: KeyCode.CODE_A,
+					},
+					DropBomb: {
+						label: 'Space',
+						keyCode: KeyCode.CODE_SPACE,
+					},
 				}}
 			/>
 			<Spacer spacing="10" />
 			<PlayerSetupContainer
 				playerId="P2"
 				keyboardConfig={{
-					MoveUp: '↑',
-					MoveLeft: '←',
-					MoveDown: ' ↓',
-					MoveRight: '→',
-					DropBomb: ';',
+					MoveUp: {
+						label: '↑',
+						keyCode: KeyCode.CODE_UP,
+					},
+					MoveRight: {
+						label: '→',
+						keyCode: KeyCode.CODE_RIGHT,
+					},
+					MoveDown: {
+						label: ' ↓',
+						keyCode: KeyCode.CODE_DOWN,
+					},
+					MoveLeft: {
+						label: '←',
+						keyCode: KeyCode.CODE_LEFT,
+					},
+					DropBomb: {
+						label: ';',
+						keyCode: KeyCode.CODE_SEMICOLON,
+					},
 				}}
 				side="right"
 			/>
